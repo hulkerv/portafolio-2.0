0597351f6d85c08c6900226414cc69c1b3a13c39
Hulker <vegat.kevinj@gmail.com>
2022-04-27 20:31:53 -0400

    media-queries(320/440/576/768) 27-04-2022

--- js/menuExpanded.js
+++ js/menuExpanded.js
@@ -2,15 +2,14 @@
 
 const expandButton = document.querySelector(".navbar-hamburguer");
 const menu = document.querySelector(".navigation");
-const mainContent = document.querySelector(".main-content")
+const mainContent = document.querySelector(".main")
 
 expandButton.addEventListener("click", () => {
   if (menu.classList.contains("collapsed")){
     menu.classList.remove("collapsed")
-  mainContent.classList.add("main-content-expand")
+  mainContent.classList.add("main-expand")
   }else {
     menu.classList.add("collapsed");
-    mainContent.classList.remove("main-content-expand")
+    mainContent.classList.remove("main-expand")
   }
 });
-
